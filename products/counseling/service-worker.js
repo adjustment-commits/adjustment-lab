061973bb422845cb20811e2b10dcf749c0209187
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-23 16:36:49 +0900

    Create service-worker.js

---
+++ products/counseling/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "counseling-v1";
+const urlsToCache = [
+  "./index.html",
+  "./style.css",
+  "./main.js",
+  "./manifest.json"
+];
+
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// リクエストをキャッシュから取得
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 古いキャッシュ削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
