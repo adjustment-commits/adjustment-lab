cb3f70bbd91ff70242a1b44ff3de5e46d716ce6a
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-28 11:35:26 +0900

    Create service-worker.js

---
+++ products/apps/base-template/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "adjustment-lab-app-base-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "/adjustment-lab/products/icons/icon-192.png",
+  "/adjustment-lab/products/icons/icon-512.png"
+];
+
+// インストール時キャッシュ
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// オフライン対応
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 古いキャッシュ削除
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
