bce70b934c787f7e5fd3fd1bd8d563cde06f8417
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-11-10 20:40:26 +0900

    Create service-worker.js

---
+++ craft/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "adjustment-main-v2";
+
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./assets/logo-sei.png",
+  "./assets/icon-field.png",
+  "./assets/icon-lab.png",
+  "./assets/icon-craft.png",
+  "./field/index.html",
+  "./lab/index.html",
+  "./craft/index.html"
+];
+
+// インストール時にキャッシュ登録
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// フェッチ時のキャッシュ処理
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 古いキャッシュの削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
